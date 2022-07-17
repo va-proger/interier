4febd4663e260ff1ed053e28c1725982a130f5ff
Vikulov Andrey <vikulov.prog@gmail.com>
2022-07-17 21:26:28 +0400

    hero, slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
+  // import Swiper bundle with all modules installed
+  import Swiper from 'swiper/bundle';
+
+  // import styles bundle
+//   import 'swiper/css/bundle';
+
+  // init Swiper:
+  const swiper = new Swiper(
+    '.hero-swiper',
+    {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+    }
+  );
+
+  // init Swiper:
 
